39ce4c7757c02a1192315c6fbaed81ac7469d1a9
rs36 <rs36@arwmtpctxap269.mw.na.cat.com>
2020-12-10 20:24:34 +0530

    another_table_creation

--- tab_fol/fol_3/insert2_table.sql
+++ tab_fol/fol_3/insert2_table.sql
@@ -1,2 +1,9 @@
-insert into id1 (p_id,p_descrp)
+insert into id2 (p_id,p_descrp)
 values (301,'car'),(302,'television'),(303,'bicycle'),(304,'books');
+
+merge into id1 using id2 
+    on id1.p_id = id2.p_id
+    when matched then 
+    update set id1.p_descrp = id2.p_descrp
+    when not matched then 
+        insert (p_id, p_descrp) values (id2.p_id, id2.p_descrp);
