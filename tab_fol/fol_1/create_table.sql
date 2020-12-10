bc8984259ed1eec5336b7f45ed1a59b16f8e4f23
rs36 <rs36@arwmtpctxap269.mw.na.cat.com>
2020-12-10 20:21:43 +0530

    Inserted another table

--- tab_fol/fol_1/create_table.sql
+++ tab_fol/fol_1/create_table.sql
@@ -1,1 +1,3 @@
-create or replace table id1 (p_id integer, p_descrp varchar, cdc_code varchar);
+create or replace table id1 (p_id integer, p_descrp varchar);
+
+create or replace  table id2 (p_id integer, p_descrp varchar);
